85a33010406d0bc511ac21f3966dead706994a6b
Hexagon-Midis <69600273+Hexagon-Midis@users.noreply.github.com>
2020-11-07 15:14:30 -0500

    Update TheHelper1.3.58.bat

--- The-Helper/TheHelper1.3.58.bat
+++ The-Helper/TheHelper1.3.58.bat
@@ -1,5 +1,5 @@
-title Starting The Helper...
 @echo off
+title Starting The Helper...
 SETLOCAL EnableDelayedExpansion
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
   set "DEL=%%a"
