fade28b14bc4be8b56bcc12b9ebd5c159072671d
Hexagon-Midis <69600273+Hexagon-Midis@users.noreply.github.com>
2020-11-07 13:32:57 -0500

    Update README.bat

--- The-Helper/README.bat
+++ The-Helper/README.bat
@@ -1,7 +1,7 @@
 @echo off
-echo.Version 1.3.57 So far
+echo If you have read this then you dont need to open this when you download the helper...
 echo.
-echo.The Folder for "The Helper" Will be more simplified soon...
+echo.Version 1.3.58 So far
 echo.
 echo.Make sure this is in downloads...!
 echo.
@@ -9,7 +9,7 @@ echo.This is the helper, a really helpful thing to shorten your clicks
 echo.
 echo.If Windows 8.1 and Windows 10 babble about a threat, allow it, this program is 100% Virus and Threat free!
 echo.
-echo.When you have downloaded this Helper... Open "The Helper 1.3.55.bat" file then it will open all of the Visual Basic Scripts and Download the required thing...
+echo.When you have downloaded this Helper... Open "The Helper 1.3.58.bat" file then it will open all of the Visual Basic Scripts and Download the required things...
 pause
 
 
